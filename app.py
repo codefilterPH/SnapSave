5f42f9f42253516b206cb8b02aa42d9c73824a81
codefilterPH <eugenereybulahan@gmail.com>
2023-05-17 17:48:25 +0800

    feat: Change app title

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from unzip import run_unzip
 
 def create_app_window():
     window = tk.Tk()
-    window.title("Snap Unzipper")
+    window.title("Snap Save/Unzipper")
 
     label = tk.Label(window, text="Click the button to run a backup or restore a backup.")
     label.pack(pady=10)
@@ -13,7 +13,7 @@ def create_app_window():
     button = tk.Button(window, text="Run Quick Backup", command=lambda: copy_folder(message_text))
     button.pack(pady=10)
 
-    button = tk.Button(window, text="Select Backup Zip", command=lambda: run_unzip(message_text))
+    button = tk.Button(window, text="Restore Backup Zip", command=lambda: run_unzip(message_text))
     button.pack(pady=10)
 
     global message_text
